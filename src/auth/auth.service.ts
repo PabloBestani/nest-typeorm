1630980f8f3e041d63105e505a4f0d0d655dc374
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-12 15:31:37 -0300

    feat(password): passwords now encrypted

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,20 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
-
+import * as bcryptjs from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly usersService: UsersService) { }
 
-    async register(registerDto: RegisterDto) {
-        const user = await this.usersService.findOneByEmail(registerDto.email);
-        if (user) throw new BadRequestException(`Email ${registerDto.email} is not available.`);
-        return await this.usersService.create(registerDto);
+    async register({ name, email, password }: RegisterDto) {
+        const user = await this.usersService.findOneByEmail(email);
+        if (user) throw new BadRequestException(`Email ${email} is not available.`);
+        return await this.usersService.create({
+            name,
+            email,
+            password: await bcryptjs.hash(password, 10)
+        });
     }
 
     login() {
