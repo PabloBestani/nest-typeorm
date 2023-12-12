09f8e125fe8ab89ea640563c2ab0f032860574b3
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-12 18:31:22 -0300

    feat(guard): created auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
 import { 
     Body,
     Controller, 
-    Post 
+    Post,
+    Get,
+    UseGuards,
+    Request
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './guard/auth.guard';
 
 
 @Controller('auth')
@@ -20,6 +24,15 @@ export class AuthController {
     @Post('login')
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto);
+    }
+
+    @Get('profile')
+    @UseGuards(AuthGuard)
+    profile(@Request() req) {
+        return {
+            message: 'profile',
+            user: req.user
+        };
     }
 
 }
