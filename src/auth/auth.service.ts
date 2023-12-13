c583e32098348b91f5d8c81ed4ef2d20b52026a0
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-13 11:34:28 -0300

    refactor(auth): documented code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,17 +24,26 @@ export class AuthService {
     }
 
     async login({ email, password }: LoginDto) {
+        // Chequeo que exista un usuario con ese email
         const user = await this.usersService.findOneByEmail(email);
         if (!user) throw new UnauthorizedException("Invalid credentials.");
 
+        // Chequeo que la password sea la correcta
         const isPasswordValid = await bcryptjs.compare(password, user.password);
         if (!isPasswordValid) throw new UnauthorizedException("Invalid credentials.");
 
+        // Genero un token nuevo para el usuario
         const secret = new ConfigService().get<string>('JWT_SECRET');
-        const token = await this.jwtService.signAsync({ email }, { secret });
+        const payload = { email, role: user.role }
+        const token = await this.jwtService.signAsync(payload, { secret });
         return {
             token,
             email
         };
     }
+
+    async profile(email: string, role: string) {
+        return await this.usersService.findOneByEmail(email);
+    }
+
 }
--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -8,6 +8,7 @@ export class AuthGuard implements CanActivate {
   constructor(private readonly jwtService: JwtService) { };
 
 
+  // Extrae el tipo de token y su valor
   private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') || [];
     return type === 'Bearer' ? token : undefined;
@@ -15,15 +16,18 @@ export class AuthGuard implements CanActivate {
 
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    // Extraigo el token del usuario actual
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
     if (!token) throw new UnauthorizedException();
 
     try {
+      // Verifico si es un usuario autenticado
       const secret = new ConfigService().get<string>('JWT_SECRET');
       const payload = await this.jwtService.verifyAsync(token, { secret });
 
+      // Una vez autenticado, inyecto sus datos en el Request
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
