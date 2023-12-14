c78fdf80ad6289f07ecce20c05bc70ac337400b3
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-14 12:48:33 -0300

    feat(dbms): changed to Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,12 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      host: process.env.MYSQL_HOST,
-      port: +process.env.MYSQL_PORT,
+      type: 'postgres',
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
       autoLoadEntities: true,
       synchronize: true, // QUITAR ESTE al llevarlo a produccion
     }),
